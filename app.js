ec827de588ff2f4233492cc46f562a0b856052db
TusiSharma <tulsisharma449922@gmail.com>
2020-08-15 20:13:49 +0530

    app update

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ app.use(function(err, req, res, next) {
 });
 
 app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname + '/client/build/index.js'));
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 })
 module.exports = app;
